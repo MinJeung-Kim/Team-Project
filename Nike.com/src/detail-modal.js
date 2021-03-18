923e7966b0e2d3cfdf7f3e37a2029ba2e942ebbc
seoyoon park <yeggam12@gmail.com>
2021-03-18 17:57:18 +0900

    고객관리 페이지, 모달창 수정

--- Nike.com/src/detail-modal.js
+++ Nike.com/src/detail-modal.js
@@ -1,13 +1,20 @@
 'use strict';
-
-    const registerBtn = document.querySelector('.registerBtn');
-    const csCloseBtn = document.querySelector('.csCloseBtn');
-    const csModal = document.querySelector('.modalWrapper');
     
-    registerBtn.addEventListener('click', openModal)
-    function openModal() {
-        csModal.style.display = "flex";
-    }
-    csCloseBtn.addEventListener('click', function() {
-        csModal.style.display = "none";
-    })
+
+        const registerBtn = document.querySelector('.registerBtn');
+        const csCloseBtn = document.querySelector('.csCloseBtn');
+        const csModal = document.querySelector('.modalWrapper');
+
+        registerBtn.addEventListener('click', openModal)
+        function openModal() {
+            csModal.style.display = "flex";
+        };
+        csCloseBtn.addEventListener('click', function() {
+            csModal.style.display = "none";
+        });
+
+        
+
+        
+
+    
