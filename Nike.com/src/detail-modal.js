ae1d2322a3d219f5e1eb111622e51935baaf944d
seoyoon park <yeggam12@gmail.com>
2021-03-18 16:15:43 +0900

    상세보기 모달창 띄우기

---
+++ Nike.com/src/detail-modal.js
@@ -0,0 +1,13 @@
+'use strict';
+
+    const registerBtn = document.querySelector('.registerBtn');
+    const csCloseBtn = document.querySelector('.csCloseBtn');
+    const csModal = document.querySelector('.modalWrapper');
+    
+    registerBtn.addEventListener('click', openModal)
+    function openModal() {
+        csModal.style.display = "flex";
+    }
+    csCloseBtn.addEventListener('click', function() {
+        csModal.style.display = "none";
+    })
