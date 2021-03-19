c4428e5d39c41b5b9651cae6e7a98c89a838b13f
seoyoon park <yeggam12@gmail.com>
2021-03-19 16:24:40 +0900

    이미지 mouseover 추가

--- Nike.com/src/category.js
+++ Nike.com/src/category.js
@@ -1,16 +1,43 @@
 'use strict';
-
-const menuClick = function(){
-	$.ajax({
-		type: 'GET', //or POST
-		url: './category-w.html',
-        dataType: 'html',
-		success: function(data) {
-			$('.right-content-wrap').html('');
-            $('.right-content-wrap').append(data);
-		}, 
-        error: function(error) {
-			alert(error);
+	//여성 상품으로 변경
+	const menuClick = function(){
+		$.ajax({
+			type: 'GET', //or POST
+			url: './category-w.html',
+			dataType: 'html',
+			success: function(data) {
+				$('.right-content-wrap').html('');
+				$('.right-content-wrap').append(data);
+			}, 
+			error: function(error) {
+				alert(error);
+			}
+		});
+	};
+	
+	//이미지 mouseover
+	const big1 = document.getElementById('big1');
+	const smallImg = document.querySelector('.smallImg');
+	const sImgfirst = smallImg.querySelector('img');
+	const sImg = smallImg.querySelectorAll('img'); //배열 리턴
+	
+	function appear() {
+		smallImg.style.display = "block";
+	}
+	function disappear() {
+		smallImg.style.display = "none";
+	}
+	
+	for(let i = 0; i < sImg.length; i++) {
+		sImg[i].onmouseover = function() {
+			big1.src=this.src;
 		}
-	});
-};
+		sImg[i].onmouseout = function() {
+			big1.src=sImgfirst.src;
+		}
+	}
+	
+	big1.addEventListener('mouseover', appear);
+	big1.addEventListener('mouseout', disappear);
+
+
