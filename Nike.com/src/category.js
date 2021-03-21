ecfb14dfeffbc003aab4998df7aa7c8007163210
seoyoon park <yeggam12@gmail.com>
2021-03-21 19:30:34 +0900

    여성페이지 내용 가져오기 수정

--- Nike.com/src/category.js
+++ Nike.com/src/category.js
@@ -1,19 +1,38 @@
 'use strict';
+
+	// const menuClick = function(){
+	// 	$.ajax({
+	// 		type: 'GET', //or POST
+	// 		url: './category-w.html',
+	// 		dataType: 'html',
+	// 		success: function(data) {
+	// 			$('.right-content-wrap').html('');
+	// 			$('.right-content-wrap').append(data);
+	// 		}, 
+	// 		error: function(error) {
+	// 			alert(error);
+	// 		}
+	// 	});
+	// };
+	
 	//여성 상품으로 변경
-	const menuClick = function(){
-		$.ajax({
-			type: 'GET', //or POST
-			url: './category-w.html',
-			dataType: 'html',
-			success: function(data) {
-				$('.right-content-wrap').html('');
-				$('.right-content-wrap').append(data);
-			}, 
-			error: function(error) {
-				alert(error);
-			}
-		});
-	};
+	const woman = document.getElementById('woman');
+	const url="./category-w.html";
+	const content = document.querySelector('.right-content-wrap');
+
+	const moveWoman = () => {
+	fetch(url)
+	.then(response => response.text())
+	.then(response => {
+		content.innerHTML = '';
+		content.innerHTML = response;
+		console.log(response);
+	})
+	.catch(error => console.log('Error :', error));
+	}
+
+	woman.addEventListener('click', moveWoman);
+
 	
 	//이미지 mouseover
 	const big1 = document.getElementById('big1');
