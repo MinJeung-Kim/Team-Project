b6aff15b7564e947aeaaa36d01a6b9cbc39ff020
seoyoon park <yeggam12@gmail.com>
2021-03-24 00:54:20 +0900

    우편번호찾기 api 추가

--- Nike.com/src/detail-modal.js
+++ Nike.com/src/detail-modal.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// 모달창 띄우기
+
     const csModal = document.querySelector('.modalWrapper');
     const registerBtnWrap = document.querySelector('.registerBtnWrap');
     const csCloseBtn =  document.querySelector('.csCloseBtn');
@@ -11,4 +13,18 @@
     registerBtnWrap.addEventListener('click', handleModal);
     csCloseBtn.addEventListener('click', handleModal);
 
+// 우편번호 검색
+    
+    const postBtn = document.querySelector('.postBtn');
+    const post = document.querySelector('.post');
+    const addrDetail = document.querySelector('.addrDetail');
+    const openZipSearch = () => {
+        new daum.Postcode({
+            oncomplete: function(data) {
+                post.value = data.zonecode;
+                addrDetail.value = data.address;
+            }
+        }).open();
+    }
 
+    postBtn.addEventListener("click", openZipSearch);
