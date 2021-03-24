e086f06d0646d4fd19690fe20a152229318e1835
seoyoon park <yeggam12@gmail.com>
2021-03-24 20:18:36 +0900

    더보기 클릭이벤트 추가

--- Nike.com/src/category.js
+++ Nike.com/src/category.js
@@ -1,44 +1,38 @@
 'use strict';
-
-	// const menuClick = function(){
-	// 	$.ajax({
-	// 		type: 'GET', //or POST
-	// 		url: './category-w.html',
-	// 		dataType: 'html',
-	// 		success: function(data) {
-	// 			$('.right-content-wrap').html('');
-	// 			$('.right-content-wrap').append(data);
-	// 		}, 
-	// 		error: function(error) {
-	// 			alert(error);
-	// 		}
-	// 	});
-	// };
 	
+	//더보기
+	const seeMore = document.querySelector('.seeMore');
+	const nFirst = document.querySelector('.nFirst');
+	const staged = document.querySelector('.staged');
+	const more = function() {
+		nFirst.insertBefore(staged, nFirst.lastElementChild);
+		seeMore.innerHTML='접기';
+	}
+	
+	seeMore.addEventListener('click',more);
+
 	//여성 상품으로 변경
 	const woman = document.getElementById('woman');
 	const url="./category-w.html";
 	const content = document.querySelector('.right-content-wrap');
-
-	const moveWoman = () => {
-	fetch(url)
-	.then(response => response.text())
-	.then(response => {
-		content.innerHTML = '';
-		content.innerHTML = response;
-		console.log(response);
-	})
-	.catch(error => console.log('Error :', error));
+	async function getWomanData() {
+		try {
+			const getData = await fetch(url);
+			const applyData = await getData.text();
+			content.innerHTML = '';
+			content.innerHTML = applyData;
+			console.log(applyData);
+		}catch {
+			error => console.log('Error :', error);
+		}	
 	}
-
-	woman.addEventListener('click', moveWoman);
+	woman.addEventListener('click', getWomanData);
 
 	
 	//이미지 mouseover
 	const bigImg = document.querySelectorAll('.category-content-img');
 		//큰 이미지 배열을 bigImg라는 변수에 넣음
-	let i = 0;
-	for(i = 0; i < bigImg.length; i++) { //bigImg의 개수만큼 for문 돌리기
+	for(let i = 0; i < bigImg.length; i++) { //bigImg의 개수만큼 for문 돌리기
 		bigImg[i].addEventListener('mouseover', function() {
 			console.log(this.parentNode.parentNode.querySelector('.smallImg'));
 				//여기서 this는 내가 mouseover한 큰 사진을 가리킴
