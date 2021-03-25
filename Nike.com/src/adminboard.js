6094fe8292078ce241cb43e86a06f13c379361ff
bom318 <kimu_u@naver.com>
2021-03-26 02:28:51 +0900

    "체크박스 추가, 그리드로 변경, 날짜필터 js 작성, css hover 추가"

--- Nike.com/src/adminboard.js
+++ Nike.com/src/adminboard.js
@@ -1,8 +1,13 @@
-new gridjs.Grid({
+ new gridjs.Grid({
   columns: [
     {
       id: 'myCheckbox',
-      name: '',
+      name: gridjs.html('<input type="checkbox"'+
+      ' style="margin-left:-50px;margin-top:-5px;position:absolute;"'+
+       'name="chk" id="inputA">'),
+      formatter: (cell,row) => gridjs.html(
+          `<input type="checkbox" style="margin-left:-50px;position:absolute;" name="chk">`
+      ),
       plugin: {
         // install the RowSelection plugin
         component: gridjs.selection.RowSelection,
@@ -17,7 +22,6 @@ new gridjs.Grid({
         enabled:false
       }
     },
-     'NO',
     {
       name: '상품코드',
       sort: {
@@ -35,7 +39,12 @@ new gridjs.Grid({
     name: '내용',
     sort: {
       enabled:false
-    }
+    },
+    width: '200px',
+    height:'44px',
+    formatter: (data) => gridjs.html (
+      `<a style="overflow:hidden;height:44px;">${data}</a>`
+    )
   },
   {
     name: '작성자',
@@ -51,7 +60,6 @@ new gridjs.Grid({
   sort: true,
   data:  [
     [
-      '7',
       '10000-265942',
       '★★★★★',
       '좋아여',
@@ -60,12 +68,12 @@ new gridjs.Grid({
       '2021-03-16',
       '1'
     ],
-    ['6','11111-565654', '★★★', '그저그럼', '생각보단 별로', '김깐깐', '2021-03-14', '16'],
-    ['5','12625-120052', '★★★★', '이쁘네요', '무난하니 이뻐요', '이평범', '2021-03-13', '10'],
-    ['4','16259-756498', '★', '너무하네요', '배송이 이틀이나걸림ㅡㅡ', '김파탄', '2021-03-01', '126'],
-    ['3','19562-775986', '★★★★★', '착한가격', '천산줄', '나천사', '2021-02-26','21'],
-    ['2','13359-126498', '★★', '귀여움', 'ㅇㅇ', '김미역', '2021-02-22', '312'],
-    ['1','19662-775966', '★★★★★', '잘입을게요', '좋네요', '한만족', '2021-02-18','19'],
+    ['11111-565654', '★★★', '그저그럼', '생각보단 별로', '김깐깐', '2021-03-14', '16'],
+    ['12625-120052', '★★★★', '이쁘네요', '무난하니 이뻐요', '이평범', '2021-03-13', '10'],
+    ['16259-756498', '★', '너무하네요', '배송이 이틀이나걸림ㅡㅡ', '김파탄', '2021-03-01', '126'],
+    ['19562-775986', '★★★★★', '착한가격', '천산줄', '나천사', '2021-02-26','21'],
+    ['13359-126498', '★★', '귀여움', 'ㅇㅇ', '김미역', '2021-02-22', '312'],
+    ['19662-775966', '★★★★★', '잘입을게요', '좋네요', '한만족', '2021-02-18','19'],
   ],
   search: {
     enabled:true
@@ -76,23 +84,24 @@ new gridjs.Grid({
     limit: 5,
     summary: false
   },
-  className: {
-    td: 'grid-td',
-    table: 'grid-tab',
-    container: 'grid-con',
-    th: 'grid-th'
+  style: {
+    table: {
+      'text-align': 'center'
+    }
   }
 }).render(document.getElementById('wrapper'));
 
 
 
-grid.on('ready', () => {
-  // find the plugin with the give plugin ID
-  const checkboxPlugin = grid.config.plugin.get('awesomeCheckbox');
+// grid.on('ready', () => {
+//   // find the plugin with the give plugin ID
+//   const checkboxPlugin = grid.config.plugin.get('awesomeCheckbox');
   
-  // subscribe to the store events
-  checkboxPlugin.props.store.on('updated', function (state, prevState) {
-    console.log('checkbox updated', state, prevState);
-  });
-})
+//   // subscribe to the store events
+//   checkboxPlugin.props.store.on('updated', function (state, prevState) {
+//     console.log('checkbox updated', state, prevState);
+//   });
+// })
+
+
 
