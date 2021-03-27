fe17f95e5f1d4e06399f20a6dc5cf6531ebc753a
MinJeung-Kim <focso5@knou.ac.kr>
2021-03-27 15:33:45 +0900

    add set date

--- Nike.com/src/order-datePicker.js
+++ Nike.com/src/order-datePicker.js
@@ -1,5 +1,10 @@
 'use strict';
 
-let orderCalender = document.querySelector('.order-calender');
+let orderEnd = document.querySelector('.order-end');
+let orderStart = document.querySelector('.order-start');
 
-orderCalender.value = new Date().toISOString().substring(0, 10);
+let date = new Date();
+let beforeMonth = new Date(Date.parse(date) - 30 * 1000 * 60 * 60 * 22); //30일전
+
+orderStart.value = beforeMonth.toISOString().substring(0, 10);
+orderEnd.value = date.toISOString().substring(0, 10); // today
