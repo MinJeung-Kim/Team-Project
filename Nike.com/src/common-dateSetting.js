40c4402a2e47f4c206621c1e4b8566f739916b4b
MinJeung-Kim <focso5@knou.ac.kr>
2021-03-27 22:29:37 +0900

    Add date button click event

--- Nike.com/src/common-dateSetting.js
+++ Nike.com/src/common-dateSetting.js
@@ -4,8 +4,25 @@
 const dateEnd = document.querySelector('.date-end');
 const dateStart = document.querySelector('.date-start');
 
+const today = document.querySelector('.day-today');
+const threeDay = document.querySelector('.day-three');
+const month = document.querySelector('.day-month');
+
 let date = new Date();
-let beforeMonth = new Date(Date.parse(date) - 30 * 1000 * 60 * 60 * 22); //30일전
+let beforeMonth = new Date(Date.parse(date) - 30 * 1000 * 60 * 60 * 24); //한 달전
+let beforeThreeDay = new Date(Date.parse(date) - 3 * 1000 * 60 * 60 * 24); //3일전
 
 dateStart.value = beforeMonth.toISOString().substring(0, 10);
 dateEnd.value = date.toISOString().substring(0, 10); // today
+
+today.addEventListener('click', function (event) {
+  dateStart.value = date.toISOString().substring(0, 10);
+});
+
+threeDay.addEventListener('click', function (event) {
+  dateStart.value = beforeThreeDay.toISOString().substring(0, 10);
+});
+
+month.addEventListener('click', function (event) {
+  dateStart.value = beforeMonth.toISOString().substring(0, 10);
+});
