3de1c546cc87dd3a8dcaf5c07f9f393159fa33a1
bom318 <kimu_u@naver.com>
2021-03-27 22:15:25 +0900

    "필터 체크박스로 변경,토글 시도"

--- Nike.com/src/review.js
+++ Nike.com/src/review.js
@@ -2,20 +2,37 @@
 
 // 최신,도움순 클릭 이벤트
 
-const filter1 = document.getElementById('filter1');
-const filter2 = document.getElementById('filter2');
+// const filter1 = document.getElementById('filter1');
+// const filter2 = document.getElementById('filter2');
+// const reviewsort = document.querySelector('.review_sort');
 
-const filter2Clicked = () => {
-    filter1.classList.remove('clicked');
-    filter2.classList.add('clicked');
-}
-const filter1Clicked = () => {
-    filter2.classList.remove('clicked');
-    filter1.classList.add('clicked');
-}
-filter2.addEventListener('click',filter2Clicked);
-filter1.addEventListener('click',filter1Clicked);
+//버블링 이용
+// reviewsort.addEventListener('click', () => {
+//     event.target.classList.toggle('clicked');
+// },true)
 
+
+// const filter2Clicked = () => {
+//     filter1.classList.remove('clicked');
+//     filter2.classList.add('clicked');
+// }
+// const filter1Clicked = () => {
+//     filter2.classList.remove('clicked');
+//     filter1.classList.add('clicked');
+// }
+// filter2.addEventListener('click',filter2Clicked);
+// filter1.addEventListener('click',filter1Clicked);
+
+//체크박스 선택시 라벨 디자인 변경
+
+const chk_input = document.querySelector('.chk_input');
+const chk_lb = document.querySelector('.chk_label');
+const reviewsort = document.querySelector('.review_sort');
+
+reviewsort.addEventListener('click' , () => {
+    event.target.classList.toggle('clicked');
+    event.stopPropagation;
+})
 
 //더보기
 
