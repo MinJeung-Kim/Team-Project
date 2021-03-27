9a2ea3f1a84e6df5f2e24011d8c28a576211b063
MinJeung-Kim <focso5@knou.ac.kr>
2021-03-27 23:28:33 +0900

    Add price slider

--- Nike.com/src/admin-orderMng.js
+++ Nike.com/src/admin-orderMng.js
@@ -1,105 +1,20 @@
-'user strict';
+'use strict';
 
-const grid = new gridjs.Grid({
-  search: true,
-  pagination: {
-    limit: 2,
-  },
-  language: {
-    search: {
-      placeholder: '🔍 Search...',
-    },
-    pagination: {
-      previous: '⬅️',
-      next: '➡️',
-      showing: '😃 Displaying',
-      results: () => 'Records',
-    },
-  },
-  sort: {
-    multiColumn: false,
-    server: {
-      url: (prev, columns) => {
-        if (!columns.length) return prev;
+var slider = document.getElementById('slider');
 
-        const col = columns[0];
-        const dir = col.direction === 1 ? 'asc' : 'desc';
-        let colName = [
-          'payDt',
-          'userNm',
-          'sendNum',
-          'sendPrice',
-          'prdNm',
-          'orderCnt',
-          'prdPrice',
-        ][col.index];
+noUiSlider.create(slider, {
+  start: [20, 80],
+  connect: true,
+  range: {
+    min: 0,
+    max: 100,
+  },
+});
 
-        return `${prev}&order=${colName}&dir=${dir}`;
-      },
-    },
-  },
-  columns: [
-    {
-      id: 'awesomeCheckbox',
-      name: 'Select',
-      width: '30px',
-      plugin: {
-        component: gridjs.selection.RowSelection,
-        props: {
-          id: (row) => row.cell(2).data,
-        },
-      },
-    },
-    '주문일(결제일)',
-    '주문번호(주문자)',
-    '운송장정보(송장번호)',
-    {
-      name: '배송비',
-      formatter: (cell, row) => {
-        return gridjs.h(
-          'button',
-          {
-            className:
-              'py-2 mb-4 px-4 border rounded-md text-white bg-blue-600',
-            onClick: () =>
-              alert(`배송비 "${row.cells[0].data}" "${row.cells[1].data}"`),
-          },
-          '2500'
-        );
-      },
-    },
-    '상품명/옵션',
-    '수량',
-    '판매가(상품구매금액)',
-    {
-      name: '총 금액',
-      data: null,
-      formatter: (_, row) =>
-        `${(row.cells[4].data + row.cells[7].data).toLocaleString()} 원`,
-    },
-    {
-      name: 'Image',
-      width: '50px',
-      sort: false,
-      formatter: (img) =>
-        gridjs.html(
-          `<center><img style="width: 100px;" src='${img}'/></center>`
-        ),
-    },
-  ],
-  server: {
-    url: 'data/order-data.json',
-    then: (data) =>
-      data.data.map((order) => [
-        order.payDt,
-        order.userNm,
-        order.sendNum,
-        order.sendPrice,
-        order.prdNm,
-        order.orderCnt,
-        order.prdPrice,
-        order.image_uris.small,
-      ]),
-    total: (data) => data.total_cards,
-  },
-}).render(document.getElementById('wrapper'));
+var nonLinearStepSliderValueElement = document.getElementById(
+  'slider-non-linear-step-value'
+);
+
+slider.noUiSlider.on('update', function (values) {
+  nonLinearStepSliderValueElement.innerHTML = values.join(' - ');
+});
