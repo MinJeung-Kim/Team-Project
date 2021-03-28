592a7614550a6215618185c6da3b8efe1bb83d5b
seoyoon park <yeggam12@gmail.com>
2021-03-28 18:40:06 +0900

    고객 테이블 이름 클릭시 수정창띄우기 수정

--- Nike.com/src/detail-modal.js
+++ Nike.com/src/detail-modal.js
@@ -6,28 +6,46 @@
     const registerBtnWrap = document.querySelector('.registerBtnWrap');
     const csCloseBtn =  document.querySelector('.csCloseBtn');
 
-    const handleModal = function() {
+    const status = document.querySelectorAll('.status-un');
+    const update = document.querySelector('.update');
+    const csRegister = document.querySelector('.csRegister');
+
+    const handleModal = (test) => { 
+        if(test == 'register') {
+            status.forEach(cStatus => {
+                cStatus.style.display = "none";
+            })
+            update.style.display = "none";
+            csRegister.textContent = "고객 등록";
+        } else if(test == "update") {
+            status.forEach(cStatus => {
+                cStatus.style.display = "block";
+            })
+            update.style.display = "inline";
+            csRegister.textContent = "고객 정보";
+        }
         csModal.classList.toggle('unstaged');
         document.body.classList.toggle('back');
         getDate();
     } 
-    registerBtnWrap.addEventListener('click', handleModal);
-    csCloseBtn.addEventListener('click', handleModal);
+    registerBtnWrap.addEventListener('click', event => handleModal('register'));
+    csCloseBtn.addEventListener('click', event => handleModal('register'));
+
 
 //가입날짜, 수정날짜
 
     const joinDate = document.querySelector('.joinDate');
     const updateDate = document.querySelector('.updateDate');
     const getDate = () => {
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = ("0" + (1 + date.getMonth())).slice(-2);
-    const day = ("0" + date.getDate()).slice(-2);
+        const date = new Date();
+        const year = date.getFullYear();
+        const month = ("0" + (1 + date.getMonth())).slice(-2);
+        const day = ("0" + date.getDate()).slice(-2);
 
-    const getToday = year + "-" + month + "-" + day;
-    updateDate.innerHTML=getToday;
-    joinDate.innerHTML=getToday;
-}
+        const getToday = year + "-" + month + "-" + day;
+        updateDate.innerHTML=getToday;
+        joinDate.innerHTML=getToday;
+    }
 
 // 우편번호 검색
     
