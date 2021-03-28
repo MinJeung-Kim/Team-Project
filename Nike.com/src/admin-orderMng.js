bab1d37334fcd85488de5d77956101ff98c9b399
MinJeung-Kim <focso5@knou.ac.kr>
2021-03-28 12:42:48 +0900

    Fix search grid

--- Nike.com/src/admin-orderMng.js
+++ Nike.com/src/admin-orderMng.js
@@ -1,20 +1,26 @@
 'use strict';
 
-var slider = document.getElementById('slider');
+var pipsSlider = document.getElementById('slider');
 
-noUiSlider.create(slider, {
-  start: [20, 80],
-  connect: true,
+noUiSlider.create(pipsSlider, {
+  start: [0, 90],
+  step: 30,
   range: {
     min: 0,
-    max: 100,
+    max: 210,
+  },
+  pips: {
+    mode: 'steps',
+    density: 3,
+    format: wNumb({
+      decimals: 2,
+      prefix: '$',
+    }),
   },
 });
 
-var nonLinearStepSliderValueElement = document.getElementById(
-  'slider-non-linear-step-value'
-);
+var nonLinearStepSliderValueElement = document.getElementById('slider-value');
 
-slider.noUiSlider.on('update', function (values) {
+pipsSlider.noUiSlider.on('update', function (values) {
   nonLinearStepSliderValueElement.innerHTML = values.join(' - ');
 });
