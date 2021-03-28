88a953f7bb2aaeff4799f3f2fb07288b5d2b5719
MinJeung-Kim <focso5@knou.ac.kr>
2021-03-28 21:22:19 +0900

    Fix date satting common code

--- Nike.com/src/admin-orderMng.js
+++ Nike.com/src/admin-orderMng.js
@@ -1,5 +1,34 @@
 'use strict';
+import { getDate } from './comm-date.js';
 
+// 날짜 자동 세팅
+const dateEnd = document.querySelector('.date-end');
+const dateStart = document.querySelector('.date-start');
+
+const today = document.querySelector('.day-today');
+const threeDay = document.querySelector('.day-three');
+const month = document.querySelector('.day-month');
+
+let date = new Date();
+let beforeMonth = getDate(-30);
+let beforeThreeDay = getDate(-3);
+
+dateStart.value = beforeMonth.toISOString().substring(0, 10);
+dateEnd.value = date.toISOString().substring(0, 10); // today
+
+today.addEventListener('click', function (event) {
+  dateStart.value = date.toISOString().substring(0, 10);
+});
+
+threeDay.addEventListener('click', function (event) {
+  dateStart.value = beforeThreeDay.toISOString().substring(0, 10);
+});
+
+month.addEventListener('click', function (event) {
+  dateStart.value = beforeMonth.toISOString().substring(0, 10);
+});
+
+//금액 설정 슬라이더
 var pipsSlider = document.getElementById('slider');
 
 noUiSlider.create(pipsSlider, {
