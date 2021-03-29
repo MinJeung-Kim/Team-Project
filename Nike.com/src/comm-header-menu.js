2cbad6b7d980a209ed0d29611098a12ee05f0163
MinJeung-Kim <focso5@knou.ac.kr>
2021-03-29 21:55:49 +0900

    Fix header menu js

--- Nike.com/src/comm-header-menu.js
+++ Nike.com/src/comm-header-menu.js
@@ -1,23 +1,36 @@
 'use strict';
 
+// header 서브메뉴 열고 닫기
 const gnvNavLi = document.querySelectorAll('.gnv__nav > li');
-const gnvListLi = document.querySelectorAll('.locator-black');
+const gnvSub = document.querySelectorAll('.gnv-sub');
 
-const gnvSub = document.querySelector('.gnv-sub');
+const releases = document.querySelector('.releases');
+const men = document.querySelector('.men');
 
 gnvNavLi.forEach(function (userItem) {
   userItem.addEventListener('mouseenter', mouseEntEvent);
 });
 
-gnvSub.addEventListener('mouseleave', mouseLeaveEvent);
+gnvSub.forEach(function (userItem) {
+  userItem.addEventListener('mouseleave', mouseLeaveEvent);
+});
 
 function mouseEntEvent(event) {
   console.log(event.target.textContent);
-  gnvSub.style.display = 'block';
-  gnvNavLi.style.cssText = 'border-bottom: 3px solid #111';
+  switch (event.target.textContent) {
+    case 'New Releases':
+      releases.className = 'on';
+      break;
+    case 'Men':
+      men.className = 'on';
+      break;
+  }
+  // gnvSub.style.display = 'block';
+  // gnvNavLi.style.cssText = 'border-bottom: 3px solid #111';
 }
 
 function mouseLeaveEvent(event) {
-  gnvSub.style.display = 'none';
-  gnvNavLi.style.cssText = '';
+  if (event.target.className === 'on') event.target.className = 'off';
+  // gnvSub.style.display = 'none';
+  // gnvNavLi.style.cssText = '';
 }
