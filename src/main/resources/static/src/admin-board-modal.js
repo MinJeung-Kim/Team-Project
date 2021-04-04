266c316d890fdd6bf8cc7f32fe70b9db81702fe2
bom <kimu_u@naver.com>
2021-04-04 22:10:51 +0900

    "모달 class추가/삭제로 자바스크립트 변경,모달 닫으면 초기화기능 추가"

--- src/main/resources/static/src/admin-board-modal.js
+++ src/main/resources/static/src/admin-board-modal.js
@@ -1,14 +1,17 @@
 'use strict'
 
+//모달
 function onClick() {
-    document.querySelector('.write_modal_back').style.display = 'block';
-    document.querySelector('.write_modal').style.display = 'block';
+    document.querySelector('.write_modal_back').classList.add('open');
+    document.querySelector('.write_modal').classList.add('open');
 }
 function offClick() {
-    document.querySelector('.write_modal_back').style.display = 'none';
-    document.querySelector('.write_modal').style.display = 'none';
+    document.querySelector('.write_modal_back').classList.remove('open');
+    document.querySelector('.write_modal').classList.remove('open');
 }
 document.getElementById('write-review').addEventListener('click',onClick);
+document.getElementById('update-review').addEventListener('click',onClick);
 document.querySelector('.write_close-button').addEventListener('click',offClick);
+
 
 
