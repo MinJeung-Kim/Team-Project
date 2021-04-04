4d7ef7f77a1df2752bca9dd8d8d2f094c976bcda
MinJeung-Kim <focso5@knou.ac.kr>
2021-04-05 00:09:08 +0900

    Add mini cart popup

---
+++ src/main/resources/static/src/cart.js
@@ -0,0 +1,52 @@
+'user strict';
+
+const btn = document.querySelector('.shopbasket');
+const close = document.querySelector('.popup-close');
+
+const body = document.querySelector('body');
+const background = document.querySelector('.popup-back');
+const popupOn = document.querySelector('.cart-popup');
+
+const img = document.querySelector('.detail-product-list > li > a > img');
+const name = document.querySelector('.title-wrap');
+const price = document.querySelector('.detail-price-text > span');
+const sizeSelect = document.querySelector('.opt-list');
+const count = document.querySelector('#numberUpDown');
+
+function styleHandler() {
+  popupOn.classList.add('on');
+  background.classList.add('on');
+  window.scrollTo(0, 0);
+  body.style.overflow = 'hidden';
+}
+
+function innerTextHandler() {
+  const miniName = document.querySelector('.mini-name');
+  const miniCount = document.querySelector('.mini-count');
+  const miniPrice = document.querySelector('.mini-price');
+  const totalPrice = document.querySelector('.total-orange');
+  const miniImg = document.querySelector('.mini-wrap > img');
+
+  miniImg.src = `${img.src}`;
+  miniName.innerText = name.textContent;
+  miniCount.innerText += count.value;
+  miniPrice.innerText = price.textContent;
+  totalPrice.innerText = price.textContent;
+}
+
+sizeSelect.addEventListener('click', (e) => {
+  const size = document.querySelector('.mini-size');
+  size.innerText += e.target.textContent;
+});
+
+// 장바구니 버튼 클릭 시  미니 장바구니 팝업 창 활성화
+btn.addEventListener('click', () => {
+  styleHandler();
+  innerTextHandler();
+});
+
+close.addEventListener('click', () => {
+  popupOn.classList.remove('on');
+  background.classList.remove('on');
+  body.style.overflow = '';
+});
