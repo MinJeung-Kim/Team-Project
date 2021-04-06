1d464a644b2d3e00b29d26f8e7e19b10488678fd
MinJeung-Kim <focso5@knou.ac.kr>
2021-04-06 17:37:39 +0900

    Send get method in url

--- src/main/resources/static/src/cart-in.js
+++ src/main/resources/static/src/cart-in.js
@@ -1,1 +1,15 @@
 'user strict';
+function getUrlParan() {
+  const url = new URL(location.href);
+  return [
+    url.searchParams.get('style'),
+    url.searchParams.get('size'),
+    url.searchParams.get('count'),
+  ];
+}
+const cartStyle = document.querySelector('.mini-style');
+const cartSize = document.querySelector('.mini-size');
+const cartCount = document.querySelector('.mini-count');
+cartStyle.innerText = getUrlParan()[0];
+cartSize.innerText = getUrlParan()[1];
+cartCount.innerText = getUrlParan()[2];
