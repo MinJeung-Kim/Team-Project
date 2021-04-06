10a6d8ab8f5a9ca0364c60163bd49dbb2222b4c6
bom <kimu_u@naver.com>
2021-04-06 16:38:23 +0900

    "모달 classList 로 변경,페이징 fetch 실패"

--- src/main/resources/static/src/review-notice.js
+++ src/main/resources/static/src/review-notice.js
@@ -3,14 +3,30 @@
 //모달
 function onClick(event) {
     if(event.target.className=='list-item-link'){
-    document.querySelector('.rv-modal-back').style.display = 'block';
-    document.querySelector('.rv-modal').style.display = 'block';
+    document.querySelector('.rv-modal-back').classList.add('open');
+    document.querySelector('.rv-modal').classList.add('open');
     }
 }
 function offClick() {
-    document.querySelector('.rv-modal-back').style.display = 'none';
-    document.querySelector('.rv-modal').style.display = 'none';
+    document.querySelector('.rv-modal-back').classList.remove('open');
+    document.querySelector('.rv-modal').classList.remove('open');
 }
 document.querySelector('.notice_list').addEventListener('click',onClick);
 document.querySelector('.rv_close-button').addEventListener('click',offClick);
 
+//페이징
+/*
+const pageNum = document.querySelector('#list2');
+const data="./notice.html";
+const notice = document.querySelector('.notice');
+
+const pageFetch = () => {
+    fetch(data)
+    .then(response => response.text())
+    .then(response => {
+        notice.innerHTML = response;
+    })
+}
+
+pageNum.addEventListener('click', pageFetch);
+*/
