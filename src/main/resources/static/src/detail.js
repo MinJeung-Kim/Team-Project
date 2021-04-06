8347bb4b8278fcc42c550b5149bbe46a65bf189f
kimyedam <kkyd6073@naver.com>
2021-04-06 23:01:38 +0900

    리뷰영역 펼치기, arrow방향 바꾸기

--- src/main/resources/static/src/detail.js
+++ src/main/resources/static/src/detail.js
@@ -1,7 +1,7 @@
 'use strict';
 
-//따로했을때는 되는데 같이 했을 때 안되는 이유
-//하나만 선택되도록 All안하면 
+//size option
+//하나만 선택되도록 >버블링으로 변경
 const inputRadio = document.querySelectorAll('.input-radio');
 
 console.log(inputRadio);
@@ -38,14 +38,10 @@ upButton.addEventListener('click', (event) => {
 
 //quantity
 const num = document.querySelector('#numberUpDown');
-
-console.log(num);
-
 const add = document.querySelector('.increaseQuantity');
 const dec = document.querySelector('.decreaseQuantity');
 
-//default값
-num.value=1;
+num.value=1;//default값
 
 add.addEventListener('click', () => {
   num.value = `${parseInt(num.value) + 1}`;
@@ -62,3 +58,20 @@ dec.addEventListener('click', () => {
     num.value=1;
   }
 });
+
+
+//review
+const dtReview = document.querySelector('#dt-review');
+const rvBox = document.querySelector('.review-box'); //보여지는 내용 받아오기
+const arrow = document.querySelector(".fa-chevron-down");
+
+//화살표 변경
+arrow.addEventListener("click", (e) => {
+  ["fa-chevron-down", "fa-chevron-up"].forEach(
+    c => e.target.classList.toggle(c));
+});
+
+// div 클릭 시 내용보여지는 것
+dtReview.addEventListener('click', () => {
+  rvBox.classList.toggle('act');
+})
