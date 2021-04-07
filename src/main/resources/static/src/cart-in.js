1a38b0e4ea5689a79d36b02e74eb33dd8964288a
MinJeung-Kim <focso5@knou.ac.kr>
2021-04-07 17:40:57 +0900

    Use local storage

--- src/main/resources/static/src/cart-in.js
+++ src/main/resources/static/src/cart-in.js
@@ -16,6 +16,16 @@ cartStyle.innerText = getUrlParan()[0];
 cartSize.innerText = getUrlParan()[1];
 cartCount.innerText = getUrlParan()[2];
 
+const cartName = document.querySelector('.mini-name');
+const cartImg = document.querySelector('.cart-wrap > img');
+const cartPrice = document.querySelectorAll('.mini-price');
+cartName.innerText = localStorage.getItem('name');
+cartImg.src = localStorage.getItem('img');
+
+cartPrice.forEach((priceNode) => {
+  priceNode.innerText = localStorage.getItem('price');
+});
+
 // 상품 삭제
 const cartClose = document.querySelector('.fa-times');
 
@@ -43,16 +53,29 @@ optionClose.addEventListener('click', () => {
 
 //선택한 사이즈 색상 변경
 const optionSize = document.querySelectorAll('.option-size span');
+const sizeDiv = document.querySelector('.option-size-warp');
 
-optionSize.forEach((seleSize) => {
-  seleSize.addEventListener('click', (e) => {
-    if (e.target) {
-      e.target.classList.add('change-color');
-    } else if (!e.target) {
-      //클릭 되지않은 노드의 change-color클래스 삭제하기
-    }
-  });
+sizeDiv.addEventListener('click', (e) => {
+  if (e.target.nodeName === 'SPAN') {
+    optionSize.forEach((option) => {
+      e.target === option
+        ? option.classList.add('change-color')
+        : option.classList.remove('change-color');
+    });
+  }
 });
+// optionSize.forEach((seleSize) => {
+//   seleSize.addEventListener('click', (e) => {
+//     // 클릭 요소 담아서 이전 클릭 요소와 현재 클릭 요소 비교
+//     const clickNode = e.target;
+//     if (clickNode === e.target) {
+//       e.target.classList.add('change-color');
+//     } else {
+//       //클릭 되지않은 노드의 change-color클래스 삭제하기
+//       e.target.classList.remove('change-color');
+//     }
+//   });
+// });
 
 // 수량 변경
 
@@ -60,11 +83,13 @@ const minus = document.querySelector('.minus');
 const puls = document.querySelector('.puls');
 const optionCount = document.querySelector('.option-count > input');
 
+const msg = document.querySelector('.msg');
 minus.addEventListener('click', () => {
+  msg.innerText = ' ';
   if (optionCount.value > 1) {
     optionCount.value = `${optionCount.value - 1}`;
   } else {
-    //클릭 못하게 방지
+    minus.classList.add('clickNone');
   }
 });
 
@@ -72,7 +97,6 @@ puls.addEventListener('click', () => {
   if (optionCount.value < 10) {
     optionCount.value = `${parseInt(optionCount.value) + 1}`;
   } else if (optionCount.value == 10) {
-    const msg = document.querySelector('.msg');
     msg.innerText = `10개 까지 구매가능 합니다.`;
   }
 });
