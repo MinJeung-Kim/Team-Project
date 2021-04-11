a0c34a4bef7d1df1698d2a94e996dd20c946f8c7
MinJeung-Kim <focso5@knou.ac.kr>
2021-04-11 23:48:00 +0900

    주문상세 정보 팝업 구현

--- src/main/resources/static/src/admin-orderMng-popup.js
+++ src/main/resources/static/src/admin-orderMng-popup.js
@@ -1,1 +1,15 @@
 'user strict';
+// 우편번호 검색
+
+const postBtn = document.querySelector('.postBtn');
+const post = document.querySelector('.post');
+const addrDetail = document.querySelector('.addrDetail');
+const openZipSearch = () => {
+  new daum.Postcode({
+    oncomplete: function (data) {
+      post.value = data.zonecode;
+      addrDetail.value = data.address;
+    },
+  }).open();
+};
+postBtn.addEventListener('click', openZipSearch);
