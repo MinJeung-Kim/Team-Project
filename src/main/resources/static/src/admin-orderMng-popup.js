95675ee992da904b7cbad92b2bdc0139e5aedc78
MinJeung-Kim <focso5@knou.ac.kr>
2021-04-12 21:10:24 +0900

    상품상세 팝업 활성화 구현

--- src/main/resources/static/src/admin-orderMng-popup.js
+++ src/main/resources/static/src/admin-orderMng-popup.js
@@ -1,6 +1,19 @@
 'user strict';
-// 우편번호 검색
 
+const popupBack = document.querySelector('.popup-back');
+const orClose = document.querySelector('.or-close');
+
+const handleModal = (test) => {
+  if (test == 'update') {
+    popupBack.classList.add('on');
+  }
+};
+
+orClose.addEventListener('click', () => {
+  popupBack.classList.remove('on');
+});
+
+// 우편번호 검색
 const postBtn = document.querySelector('.postBtn');
 const post = document.querySelector('.post');
 const addrDetail = document.querySelector('.addrDetail');
