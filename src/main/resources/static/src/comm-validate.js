b3254397ab6737efce991f2fc51ba4863f7374f7
seoyoon park <yeggam12@gmail.com>
2021-04-15 02:26:21 +0900

    필수입력 사항 검사

--- src/main/resources/static/src/comm-validate.js
+++ src/main/resources/static/src/comm-validate.js
@@ -1,29 +1,82 @@
 'use strict';
 
-
 const frm = document.getElementById('reg_form'); //form id값 가져오기
-const fName = /^[가-힣a-zA-Z]+/; //정규식
-const fEmail = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
+const regName = /^[가-힣a-zA-Z]+/; //정규식
+const regEmail = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
+const regPass = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@%^&*-]).{8,16}$/;
+const regPhone = /^[0-9]{2,3}[0-9]{3,4}[0-9]{4}/;
 
 frm.addEventListener('submit', (e) => {
     e.preventDefault();
-//이름
-    if(fName.test(frm.name.value) == true){ //test()로 유효성 검사 true
-        frm.name.style.border = "1px solid gray";
-    } else if(fName.test(frm.name.value) == false) {//test()로 유효성 검사 false
-        alert("이름은 한글과 영문만 입력 가능합니다.");
-        frm.name.value = '';
+    //이름
+    if(frm.name.value==null || frm.name.value=="") {
+        alert("이름을 입력하세요.");
         frm.name.focus();
         return false; 
+    } else {
+        if(regName.test(frm.name.value) == false) {//test()로 유효성 검사 false
+            alert("이름은 한글과 영문만 입력 가능합니다.");
+            frm.name.value = '';
+            frm.name.focus();
+            return false; 
+        } 
     }
 
-//ID(이메일)
-    if(fEmail.test(frm.email.value) == true) {
-        frm.email.style.border = "1px solid gray";
-    } else if(fEmail.test(frm.email.value) == false) {
-        alert("이메일 형태로 입력해주세요. \n이메일은 영문만 가능합니다.");
-        frm.email.value = '';
+    //ID(이메일)
+    if(frm.email.value==null || frm.email.value=="") {
+        alert("이메일을 입력하세요.");
         frm.email.focus();
-        return false;
+        return false; 
+    } else {
+        if(regEmail.test(frm.email.value) == false) {
+            alert("이메일 형태로 입력해주세요. \n이메일은 영문만 가능합니다. \n예시)abcd@efg.com");
+            frm.email.value = '';
+            frm.email.focus();
+            return false;
+        } 
+    }
+
+    //비밀번호
+    if(frm.email.value==null || frm.password.value=="") {
+        alert("비밀번호를 입력하세요.");
+        frm.password.focus();
+        return false; 
+    } else {
+        if(regPass.test(frm.password.value) == false){
+            alert("비밀번호는 영문 대 소문+숫자+특수문자 8~16자리로 입력해주세요."+
+            "\n특수문자 괄호(),<>는 사용 불가");
+            frm.password.value = '';
+            frm.password.focus();
+            return false;
+        } 
+    }
+
+    //전화번호
+    if(frm.phone.value==null || frm.phone.value=="") {
+        alert("전화번호를 입력하세요.");
+        frm.phone.focus();
+        return false; 
+    } else {
+        if(regPhone.test(frm.phone.value) == false) {
+            if(frm.phone.value.includes("-")) {
+                alert("전화번호는 -없이 입력해주세요.");
+                frm.phone.value = '';
+                frm.phone.focus();
+                return false;
+            } else {
+                alert("전화번호는 숫자만 입력 가능합니다.");
+                frm.phone.value = '';
+                frm.phone.focus();
+                return false;
+            }
+        } else if(frm.phone.value.length >= 12) {
+            alert("전화번호는 12자를 넘기 수 없습니다.");
+            frm.phone.value = '';
+            frm.phone.focus();
+            return false;
+        } else {
+            alert("등록하시겠습니까?")
+            frm.submit();
+        }
     }
 });
