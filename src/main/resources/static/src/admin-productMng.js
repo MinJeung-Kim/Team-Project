429e6f2b785c1276d2993c43e7d242312f9c069b
kimyedam <kkyd6073@naver.com>
2021-04-15 17:28:13 +0900

    슬라이더 추가(사이즈 변경해야함)

--- src/main/resources/static/src/admin-productMng.js
+++ src/main/resources/static/src/admin-productMng.js
@@ -126,3 +126,28 @@ new gridjs.Grid({
     ],
   }).render(document.getElementById('wrapper'));
   
+//금액 설정 슬라이더
+var pipsSlider = document.getElementById('slider');
+
+noUiSlider.create(pipsSlider, {
+  start: [0, 90],
+  step: 30,
+  range: {
+    min: 0,
+    max: 210,
+  },
+  pips: {
+    mode: 'steps',
+    density: 3,
+    format: wNumb({
+      decimals: 2,
+      prefix: '$',
+    }),
+  },
+});
+
+var nonLinearStepSliderValueElement = document.getElementById('slider-value');
+
+pipsSlider.noUiSlider.on('update', function (values) {
+  nonLinearStepSliderValueElement.innerHTML = values.join(' - ');
+});
