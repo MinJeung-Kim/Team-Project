4c71be4978ad094600d945a6ae44adf19d4ca215
MinJeung-Kim <focso5@knou.ac.kr>
2021-04-26 23:27:51 +0900

    Print data in pop-up window

--- src/main/resources/static/src/admin-orderMng-popup.js
+++ src/main/resources/static/src/admin-orderMng-popup.js
@@ -3,11 +3,38 @@
 const popupBack = document.querySelector('.popup-back');
 const orClose = document.querySelector('.or-close');
 
+//팝업 활성화
 const handleModal = (test) => {
   if (test == 'update') {
+    alert('dd');
     popupBack.classList.add('on');
+    open('focso5@gmail.com');
   }
 };
+
+// 팝업창에 데이터 출력
+async function open(email) {
+  const res = await fetch(`/inCart/${email}`);
+  const json = await res.json();
+
+  const title = document.querySelector('.or-content-title');
+  const style = document.querySelector('.or-content-style');
+  const size = document.querySelector('.or-content-size');
+  const count = document.querySelector('.or-content-count');
+  const price = document.querySelector('.or-content content-price');
+  const sendPrice = document.querySelector('.content-send');
+  const select = document.querySelector('#or-content-status');
+  const status = select.options[select.selectedIndex].value;
+
+  alert(status);
+
+  title.textContent = json.prdNm;
+  style.textContent = json.prdStyle;
+  size.textContent = json.prdSize;
+  count.textContent = json.prdCnt;
+  price.textContent = json.prdPrice;
+  sendPrice.textContent = json.sendPrice;
+}
 
 orClose.addEventListener('click', () => {
   popupBack.classList.remove('on');
