a8c4aed84335b4ba803edc4b60d630ca57d38aea
kimyedam <kkyd6073@naver.com>
2021-04-26 14:38:31 +0900

    수정 모달창 연결

--- src/main/resources/static/src/admin-product-modal.js
+++ src/main/resources/static/src/admin-product-modal.js
@@ -3,18 +3,28 @@
 const prdRegister=document.querySelector('.prd-register');
 const prdModalback=document.querySelector('.prd-register-back');
 const prdModal=document.querySelector('.prd-register-modal');
+const rgText=document.querySelector('.prRegister');
 console.log(prdRegister);
 
-function onClick() {
-    prdModalback.classList.add('open');
-    prdModal.classList.add('open');
+function openClick(test) {
+    
+    if(test=='register') {
+        prdModalback.classList.add('open');
+        prdModal.classList.add('open');
+    }
+    else if (test=='update') {
+        prdModalback.classList.add('open');
+        prdModal.classList.add('open');
+        rgText.textContent="상품 정보 수정";
+    }
+
 }
 function offClick() {
     prdModalback.classList.remove('open');
     prdModal.classList.remove('open');
 }
 
-document.querySelector('.prd-rgs-button').addEventListener('click',onClick);
+document.querySelector('.prd-rgs-button').addEventListener('click', event => openClick('register'));
 document.querySelector('.prd_close-button').addEventListener('click',offClick);
 
 //가입날짜
