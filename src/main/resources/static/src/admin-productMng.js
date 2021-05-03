dbeda211616e53b12937bb48df5ee3944b5dc029
kimyedam <kkyd6073@naver.com>
2021-05-03 19:02:48 +0900

    사이드메뉴 변경

--- src/main/resources/static/src/admin-productMng.js
+++ src/main/resources/static/src/admin-productMng.js
@@ -26,7 +26,7 @@ new gridjs.Grid({
       attributes: (cell, row) => {
         if (cell, row) { 
           return {
-            'onClick': () => openClick('update',row.cells[2].data),
+            'onClick': () => openClick('update',row.cells[1].data),
             //alert(`Editing "${row.cells[0].data}" "${row.cells[1].data}" "${row.cells[2].data}" "${row.cells[3].data}" "${row.cells[4].data}"`),
             'style': 'cursor: pointer',
           }
@@ -70,7 +70,7 @@ new gridjs.Grid({
       }
     },
     fixedHeader: true,
-    data: JSON.parse(adProduct).map(({prdCd,prdNm,prdPr,prdStk, insDt}) => [prdCd,prdNm,prdPr,prdStk,insDt]),
+    data: JSON.parse(adProduct).map(({prdCd, prdNm, prdPr, prdStk, insDt}) => [prdCd,prdNm,prdPr,prdStk,insDt]),
   }).render(document.getElementById("wrapper"));
   
 //금액 설정 슬라이더
